4a69c748c8a8f6115bacb82f8a0e825317e8305d
ggome <gomez.gwendo@gmail.com>
2014-01-08 08:06:44 +0100

    Phase 7

---
+++ Controleur/ControleurPersonalise.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'Framework/Controleur.php';
+require_once 'Modele/Styles.php';
+
+
+/**
+ * Classe parente des contrôleurs soumis à authentification
+ *
+ */
+abstract class ControleurPersonalise extends Controleur
+{
+    
+    public function genererVue($donnees = array(), $action = null)
+    {
+        if ($this->requete->getSession()->existeAttribut("idClient"))
+        {
+            $login = $this->requete->getSession()->getAttribut("loginClient");
+            $mdp = $this->requete->getSession()->getAttribut("mdpClient");
+            $client = $this->client->getClient($login, $mdp);
+            parent::genererVue($donnees + array('client' => $client), $action);
+        }
+        else
+            parent::genererVue($donnees, $action);
+    }
+}
